72178e1bd2e149c6c83e2f67d2e0e03a021b3666
ojasvi <ojasvikumar12031999@gmail.com>
2020-05-13 09:37:18 +0530

    loops

--- swirl.R
+++ swirl.R
@@ -1,6 +1,19 @@
 install.packages("swirl")
 packageVersion("swirl")
-library("swirl")
 ls()
 rm(list = ls())
 info()
+help.start()
+bye()
+swirl()
+2
+0
+1
+1
+my_div<-z/my_sqrt
+3
+my_div
+c(1, 2, 3, 4) + c(0, 10, 100) 
+args(list.files)
+
+
